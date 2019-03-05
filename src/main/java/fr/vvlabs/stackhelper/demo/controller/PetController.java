2200a901843307b5dd8e22ad16c04b56ef5e7187
vvillain <vvillain@questel.com>
2019-03-05 11:17:22 +0100

    no querydsl

--- src/main/java/fr/vvlabs/stackhelper/demo/controller/PetController.java
+++ src/main/java/fr/vvlabs/stackhelper/demo/controller/PetController.java
@@ -1,25 +1,12 @@
 package fr.vvlabs.stackhelper.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.querydsl.binding.QuerydslPredicate;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import com.querydsl.core.types.Predicate;
 
 import fr.vvlabs.stackhelper.controller.AbstractRestController;
 import fr.vvlabs.stackhelper.demo.dto.PetDTO;
 import fr.vvlabs.stackhelper.demo.dto.PetWriteDTO;
 import fr.vvlabs.stackhelper.demo.model.Pet;
-import fr.vvlabs.stackhelper.demo.service.PetService;
 
 @RestController
 @RequestMapping("/pets")
@@ -27,18 +14,4 @@ import fr.vvlabs.stackhelper.demo.service.PetService;
 public class PetController extends AbstractRestController<Pet, Integer, PetDTO, PetWriteDTO> {
 	// crud endpoints are already defined !
 	// add only new services
-
-	@Autowired
-	private PetService service;
-	
-	@Override
-	@GetMapping
-	public ResponseEntity<Page<PetDTO>> findAll( //
-			@QuerydslPredicate(root = Pet.class) Predicate predicate, //
-			@RequestParam(value = "page", required = false, defaultValue = "0") int page, //
-			@RequestParam(value = "size",  required = false, defaultValue = "30") int size, //
-			@RequestParam(value = "sort",  required = false) String sort //
-			) { //
-		return super.findAll(predicate, page, size, sort);
-	}
 }
